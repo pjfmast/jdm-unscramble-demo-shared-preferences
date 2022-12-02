490de45d7c9c2e63099cd072b2e2499ff676e45c
Paul de Mast <pjfmast@gmail.com>
2022-12-02 15:13:17 +0100

    this branch: Unscramble game from Android basics in Kotlin mooc without SharedPreferences for high score See branch 'use-sharedpreferences-for-high-score' for using SharedPreferences for high score

---
+++ app/src/main/java/avd/jdm/unscrambledemosharedpreferences/MainActivity.kt
@@ -0,0 +1,11 @@
+package avd.jdm.unscrambledemosharedpreferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
