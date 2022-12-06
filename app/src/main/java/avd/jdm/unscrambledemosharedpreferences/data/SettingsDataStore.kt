e915a5df4280da3a7780fe9016110b3a9985b915
Paul de Mast <pjfmast@gmail.com>
2022-12-06 14:02:06 +0100

    added datetime of highscore to SettingsDataStore.kt

--- app/src/main/java/avd/jdm/unscrambledemosharedpreferences/data/SettingsDataStore.kt
+++ app/src/main/java/avd/jdm/unscrambledemosharedpreferences/data/SettingsDataStore.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import java.io.IOException
+import java.time.LocalDateTime
 
 private const val HIGH_SCORE_PREFERENCES_NAME = "high_score_preferences"
 
@@ -19,6 +20,7 @@ val Context.dataStore : DataStore<Preferences> by preferencesDataStore(
 
 class SettingsDataStore(preference_datastore: DataStore<Preferences>) {
     private val HIGH_SCORE_VALUE = intPreferencesKey("saved_high_score_value")
+    private val HIGH_SCORE_DATETIME = stringPreferencesKey("saved_high_score_datetime")
 
     val preferenceFlow: Flow<Int> = preference_datastore.data
         .catch {
@@ -30,12 +32,14 @@ class SettingsDataStore(preference_datastore: DataStore<Preferences>) {
             }
         }
         .map { preferences ->
+            preferences[HIGH_SCORE_DATETIME] ?: ""
             // On the first run of the app, we will return a high_score value of 0 by default
             preferences[HIGH_SCORE_VALUE] ?: 0
         }
 
     suspend fun saveHighScoreToPreferencesStore(newHighScore: Int, context: Context) {
         context.dataStore.edit { preferences ->
+            preferences[HIGH_SCORE_DATETIME] = LocalDateTime.now().toString()
             preferences[HIGH_SCORE_VALUE] = newHighScore
         }
     }
